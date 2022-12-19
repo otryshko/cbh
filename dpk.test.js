98305afc8ff020548887d17669f025348f093169
Volodymyr Otryshko <volodymyrotr@gmail.com>
2022-12-19 06:58:55 -0800

    Update dpk.test.js

--- dpk.test.js
+++ dpk.test.js
@@ -24,13 +24,11 @@ describe("deterministicPartitionKey", () => {
   it("Returns stringified partition key when an object under than the max length", () => {
     const longKey = Array(127).fill().map(_ => Math.floor((Math.random() * 10)))
     const trivialKey = deterministicPartitionKey({'partitionKey': longKey});
-    console.log(longKey)
     expect(trivialKey.length).toBe(255);
   });
   it("Returns hash when an object bigger than the max length", () => {
     const longKey = Array(128).fill().map(_ => Math.floor((Math.random() * 10)))
     const trivialKey = deterministicPartitionKey({'partitionKey': longKey});
-    console.log(longKey)
     expect(trivialKey.length).toBe(128);
   });
 });
